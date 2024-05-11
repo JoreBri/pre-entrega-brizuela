b8a5e5c1e29e9b312df3f28b87f28ed2425fc0a6
JoreBri <jorebrizuela.jb@gmail.com>
2024-05-10 23:40:26 -0300

    mejoras del header

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -4,9 +4,9 @@ export const NavBar = () => {
     return (
     <nav className='nav'>
             <ul className='navMenu'>
-                <li className='navItem'><button><a className='navLink' href="#">Collares</a></button> {/* Enlace a la página de collares */}</li>
-                <li className='navItem'> <button><a className='navLink' href="#">Platos</a></button> {/* Enlace a la página de platos */}</li>
-                <li className='navItem'> <button><a className='navLink' href="#">Juguetes</a></button> {/* Enlace a la página de juguetes */}</li>
+                <li className='navItem'><button className='boton'><a className='navLink' href="#">Collares</a></button> {/* Enlace a la página de collares */}</li>
+                <li className='navItem'> <button className='boton'><a className='navLink' href="#">Platos</a></button> {/* Enlace a la página de platos */}</li>
+                <li className='navItem'> <button className='boton'><a className='navLink' href="#">Juguetes</a></button> {/* Enlace a la página de juguetes */}</li>
             </ul>
     </nav>
             )
