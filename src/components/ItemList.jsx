51e3c89d76ac54a311b0ad32c69ab839752b4c82
JoreBri <jorebrizuela.jb@gmail.com>
2024-05-18 18:05:36 -0300

    img

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Item } from './Item'
+import { ItemCounts } from './ItemCounts'
 
 export const ItemList = ( {productos} ) => {
     return (
@@ -8,6 +9,7 @@ export const ItemList = ( {productos} ) => {
             productos.length > 0 ?
             productos.map (producto => {
                 return <Item key = { producto.id} producto={producto} />
+            
             })
             : <p>No hay productos</p>
         }
