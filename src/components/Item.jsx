d0ab8e7ed0d8430b1286471090acb12c89be604c
JoreBri <jorebrizuela.jb@gmail.com>
2024-05-18 18:35:20 -0300

    img

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Item = ( {producto} ) => {
     return (
         <div className="producto">
-        <img src={producto.imagen} />
+        <img className='imgProducto' src={producto.imagen} />
         <h2>{producto.nombre}</h2>
         <p>${producto.precio}</p>
         <p>{producto.descripcion}</p>
