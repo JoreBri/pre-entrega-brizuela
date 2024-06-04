826cc489116166955cfca1673f43356253f852f3
JoreBri <jorebrizuela.jb@gmail.com>
2024-06-03 22:59:19 -0300

    logo y titulo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import { NavBar } from './NavBar'
-import { CarritoWidget } from './CarritoWidget'
+import React from 'react';
+import { NavBar } from './NavBar';
+import { CarritoWidget } from './CarritoWidget';
 import logo from '../../assets/logo.jpg'; // Importa la imagen
+import { Routes, Route, Link } from "react-router-dom";
+
 
 export const Header = () => {
   return (
-    <header className='header'> 
-    <div className='imgDiv'>
-    <a href="#"><img className='imgLogo' src={logo} alt="Ejemplo" /></a>
-    <a className='logoLink' href="#"><h1>¡PET PARADISE!</h1></a>
+    <header className='header'>
+      <div className='imgDiv'>
+        <Link to="/">
+          <img className='imgLogo' src={logo} alt="Ejemplo" />
+        </Link>
+        <Link className='logoLink' to="/"><h1>¡PET PARADISE!</h1></Link>
       </div>
       <NavBar />
       <CarritoWidget/>
-      
     </header>
-    
-  )
+  );
 }
+
